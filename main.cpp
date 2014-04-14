fb405d739cf1643abddbda02d4176b9b2f5af498
Gabriel de Dietrich <gabriel.dedietrich@me.com>
2014-04-14 16:39:21 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "qtquick2controlsapplicationviewer.h"
+
+int main(int argc, char *argv[])
+{
+    Application app(argc, argv);
+
+    QtQuick2ControlsApplicationViewer viewer;
+    viewer.setMainQmlFile(QStringLiteral("qml/RGB-Analyzer/main.qml"));
+    viewer.show();
+
+    return app.exec();
+}
